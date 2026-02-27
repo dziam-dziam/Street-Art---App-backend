0105ed9ffe7af26fb99c76725a9e22ab2d4d17f7
Damian <damianzmudzinski3@gmail.com>
2026-02-27 14:26:30 +0100

    deleted longblob from Photo entity

--- src/main/java/org/example/entities/Photo.java
+++ src/main/java/org/example/entities/Photo.java
@@ -26,7 +26,7 @@ public class Photo {
     private Long sizeBytes;
 
     @Lob
-    @Column(name = "data", nullable = false, columnDefinition = "LONGBLOB")
+    @Column(name = "data", nullable = false)
     private byte[] imageData;
 
     @ManyToOne(fetch = FetchType.LAZY)
